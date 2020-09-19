84cbc98ecdcf7a8845a9b6fdd7aa44fbf789d387
Manuel-Baumann <64980449+Manuel-Baumann@users.noreply.github.com>
2020-09-20 01:54:14 +0200

    Add files via upload

---
+++ Piece.js
@@ -0,0 +1,19 @@
+var piece;
+var color;
+var x;
+var y;
+
+class Piece{
+
+  constructor(piece, x, y, color){
+    this.piece = piece;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+  }
+
+
+  test(){
+    alert("sag");
+  }
+}
